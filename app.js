04206adb9a826b0ad215460c34cd43bea4dced92
Muhammad Hamza <hamzakabeer021@gmail.com>
2025-02-06 17:06:28 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let eliteLaptopsCollections = {
+    laptopName: "ASUS ROG",
+    laptopColor: ["Black","Grey","Red&Green"],
+    laptopModelNum: "Rog Strix G18",
+
+}
+
+let search = prompt("Enter your collections");
+
+console.log([eliteLaptopsCollections[search]]);
